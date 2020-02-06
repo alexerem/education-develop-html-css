048a9181fec3c434fda16f972ccf76d7969d5cef
Alexander <band.kist@gmail.com>
2020-02-07 00:32:55 +0300

    make adaptive header

--- htmlacademy.ru (site: "Кваст")/js/main.js
+++ htmlacademy.ru (site: "Кваст")/js/main.js
@@ -1,10 +1,14 @@
 $(function(){
 
-  $('.header-slider-jq').slick({
+ $('.header-slider-jq').slick({
     arrows: false,
     autoplay: 2000,    
     dotsClass: 'header-dots',
     dots: true
+  });
+
+  $('.main-menu__btn').on('click', function () {
+    $('.main-menu__list').slideToggle();
   });
 
 });
