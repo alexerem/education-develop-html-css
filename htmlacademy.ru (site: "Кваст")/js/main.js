ae9a1136d7e4ce0311e5f98c05a2297949ab4834
Alexander <band.kist@gmail.com>
2020-02-11 15:22:11 +0300

    make adaptiv section "news"

--- htmlacademy.ru (site: "Кваст")/js/main.js
+++ htmlacademy.ru (site: "Кваст")/js/main.js
@@ -1,8 +1,8 @@
-$(function(){
+$(function () {
 
- $('.header-slider-jq').slick({
+  $('.header-slider-jq').slick({
     arrows: false,
-    autoplay: 2000,    
+    autoplay: 4000,
     dotsClass: 'header-dots',
     dots: true
   });
@@ -10,5 +10,29 @@ $(function(){
   $('.main-menu__btn').on('click', function () {
     $('.main-menu__list').slideToggle();
   });
+
+  $(window).resize(function () {
+    if ($(window).width() < 810) {
+      $('.news-item').not('.slick-initialized').slick({
+        arrows: false,
+        autoplay: 1500,
+      });
+      $('.news__title').hide();
+    } else {
+      $('.news-item').filter('.slick-initialized').slick('unslick');
+      $('.news__title').show();
+    };
+  });
+
+  if ($(window).width() < 810) {
+    $('.news-item').slick({
+      arrows: false,
+      autoplay: 1500,
+    });
+    $('.news__title').hide();
+  } else {
+    $('.news-item').filter('.slick-initialized').slick('unslick');
+    $('.news__title').show();
+  };
 
 });
